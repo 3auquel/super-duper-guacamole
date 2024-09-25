d35de4eb101e074f697b94f89b82b99747e5fe21
Leo <3aukahaha@gmail.com>
2024-09-25 19:21:53 +0400

    Это наш первый комит

---
+++ base/script.js
@@ -0,0 +1,67 @@
+// 'use strict'
+// // console.log(leftBorderWidth);
+// // var leftBorderWidth = 1;
+// // let second = 2;
+// // const pi = 3.14;
+// // console.log(leftBorderWidth);
+// console.log(4/0);
+// console.log('string'*9);
+// var n=6;
+// var string = "hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// var obj = {};
+// let person = {
+//     name: "Jonh",
+//     age: 25,
+//     isMarried: false
+
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.isMarried);
+// console.log(person["isMarried"])
+
+// let arr = ['plam.png', 'aornge.jpeg', 'apple.bmp'];
+
+// console.log(arr[0])
+// String(null);
+// console.log(typeof(String(4)));
+// console.log(typeof("ww" + "5"));
+// console.log('htttp:/vk.com/catalog/' + 5);
+
+// console.log(typeof(Number('5')));
+// console.log(5+ +'5');
+// console.log(typeof(parseInt('15px', 10)));
+// let ans = +prompt("Hello", '');
+// console.log(ans);
+// 0, ' ', null, undefined, NaN
+// let switcher = null;
+
+// if (switcher){
+//     console.log("Working..")
+// }
+// switcher = 1;
+// if (switcher){
+//     console.log("Working..")
+// }
+// console.log(typeof(Boolean('5')));
+// console.log(typeof(!!'5'));
+// alert("Hello world!");
+
+
+// let answer = confirm("are you here?");
+// console.log(answer);
+// let answer = +prompt("are you here?", "yes");
+// console.log(typeof(answer));
+// let incr = 10,
+// decr = 10;
+// // incr++;
+// // decr--;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(3 === 2);
+let isChecker = true,
+isCloce = false;
+console.log(isChecker || isCloce);

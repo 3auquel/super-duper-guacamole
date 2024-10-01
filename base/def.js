6f99317bc78cd82ee2f02c488a5c520abc96d31f
Leo <3aukahaha@gmail.com>
2024-10-01 20:29:15 +0400

    потеряный файл

---
+++ base/def.js
@@ -0,0 +1,38 @@
+// let num = 20;
+// function showFirstMessagde(text){
+//     alert(text);
+
+//     console.log(num);
+// }
+
+
+// showFirstMessagde("далеко за полем поле");
+// console.log(num);
+// function calcul(a, b) {
+//     return (a + b);
+// }
+
+
+// function returnVar() {
+//     let num = 50;
+//     return num;
+// }
+// let anotherNum = returnVar();
+// console.log(anotherNum);
+
+// let calc = function (a, b) {
+//     return (a * b);
+// }
+// console.log(calc(2,4));
+// console.log(calcul(2,4));
+
+let calc = (a, b) => a + b
+console.log(calc(4, 6))
+let str = 'test';
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+let eleven = "12.2px";
+// console.log(Math.round(eleven));
+console.log(parseInt(eleven));
+console.log(parseFloat(eleven));
